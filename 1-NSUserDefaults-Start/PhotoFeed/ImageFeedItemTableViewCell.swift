320e8d735e7de3079c5ece3601f1113814dc7fb0
emmanuel lopez <emmanuel.lopez@simplias.com>
2021-02-09 23:36:47 -0600

    adding ImageFeedItemTable

---
+++ 1-NSUserDefaults-Start/PhotoFeed/ImageFeedItemTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  ImageFeedItemTableViewCell.swift
+//  PhotoFeed
+//
+//  Created by Emmanuel Lopez Guerrero on 09/02/21.
+//  Copyright © 2021 YourOganisation. All rights reserved.
+//
+
+import Foundation
