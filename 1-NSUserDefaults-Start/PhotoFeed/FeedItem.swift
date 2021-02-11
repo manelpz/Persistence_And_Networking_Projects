b36a468d8b2d832ed63643d603ae8f6e8f643167
emmanuel lopez <emmanuel.lopez@simplias.com>
2021-02-10 23:46:47 -0600

    Update FeedItem.swift

--- 1-NSUserDefaults-Start/PhotoFeed/FeedItem.swift
+++ 1-NSUserDefaults-Start/PhotoFeed/FeedItem.swift
@@ -8,11 +8,3 @@
 
 import Foundation
 
-
-//
-//  FeedItem.swift
-//  PhotoFeed
-//
-//  Created by Emmanuel Lopez Guerrero on 09/02/21.
-//  Copyright © 2021 YourOganisation. All rights reserved.
-//
